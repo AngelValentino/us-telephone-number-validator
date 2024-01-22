0e54c2eafbbb163bcfe66b8928d68c259f942d29
AngelValentino <138892399+AngelValentino@users.noreply.github.com>
2024-01-22 18:02:55 +0100

    Project done.

---
+++ main.js
@@ -0,0 +1,42 @@
+const checkBtnLm = document.getElementById('check-btn');
+const clearBtnLm = document.getElementById('clear-btn');
+const resultLm = document.getElementById('results-div');
+const inputLm = document.getElementById('user-input');
+
+
+function testRegex(regex, str) {
+  return regex.test(str);
+}
+
+function validateNumber() {
+  const input = inputLm.value;
+  const regex = /^(?:\+?1\s?)?(?:\(\d{3}\)|\d{3})[-\s.]?\d{3}[-\s.]?\d{4}$/
+
+  console.log(input);
+  console.log(resultLm);
+
+  if (!input) {
+    alert('Please provide a phone number');
+    return;
+  }
+
+  resultLm.innerHTML = testRegex(regex, input)
+    ? `<p><span class="valid-input">Valid</span> US number: <span>${input}<span></p>`
+    : `<p><span class="invalid-input">Invalid</span> US number: <span>${input}<span></p>`; 
+}
+
+function clearResluts() {
+  resultLm.innerHTML = '';
+  inputLm.value = '';
+}
+
+
+inputLm.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter') {
+    validateNumber();
+  }
+});
+
+checkBtnLm.addEventListener('click', validateNumber);
+
+clearBtnLm.addEventListener('click', clearResluts);
